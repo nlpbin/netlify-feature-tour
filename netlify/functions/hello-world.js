4c0a7fffe9c8666f7909ea56ad29989618a0ae20
gaob13 <gaob13@tsinghua.org.cn>
2025-03-08 22:53:39 +0800

    why

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -15,42 +15,49 @@ const dbpassword = process.env.dbpassword;
 
 exports.handler = async() => {
 
-const connection = mysql.createConnection({
-    host: `${dburl}`,
-    user: 'gaobin',
-    password: `${dbpassword}`,
-    database: 'ywh'
-});
-
-connection.connect((err) => {
+    const connection = mysql.createConnection({
+        host: `${dburl}`,
+        user: 'gaobin',
+        password: `${dbpassword}`,
+        database: 'ywh'
+    });
     var ans;
-    if (err) {
-        ans = '数据库连接失败:';
-	return ans;
-    }
-
-    const query = 'SELECT * FROM user';
-    connection.query(query, (error, results, fields) => {
-        if (error) {
-            ans = '查询出错:';
-        } else {
-            const jsonResult = JSON.stringify(results);
-            console.log('转换为 JSON 字符串的结果:', jsonResult);
-	    ans = jsonResult;
+    connection.connect((err) => {
+        var ans;
+        if (err) {
+            ans = '数据库连接失败:';
+    	    return {
+		statusCode: 200,
+		body: ans
+	    };
         }
-
-        connection.end((endErr) => {
-            if (endErr) {
-                ans = '关闭数据库连接出错:';
+    
+        const query = 'SELECT * FROM user';
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                ans = '查询出错:';
+            } else {
+                const jsonResult = JSON.stringify(results);
+                console.log('转换为 JSON 字符串的结果:', jsonResult);
+    	    ans = jsonResult;
             }
+    
+            connection.end((endErr) => {
+                if (endErr) {
+                    ans = '关闭数据库连接出错:';
+                }
+            });
         });
+        return {
+            statusCode: 200,
+            body: `hello world! I have a BUG ${pool}: ${ans}`
+        }
     });
     return {
-        statusCode: 200,
-        body: `hello world! I have a BUG ${pool}: ${ans}`
-    }
-});
-
+	statusCode: 200,
+	body: ans
+    };
+    
 }
 
 
