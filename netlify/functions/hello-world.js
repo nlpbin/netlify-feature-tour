77a5a13da4a1e2e49b1de0376eabea5613d4beaa
gaob13 <gaob13@tsinghua.org.cn>
2025-03-08 22:13:35 +0800

    ywh

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,10 +1,28 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: `${dburl}`,
+  user: 'root',
+  password: `${dbpassword}`,
+  database: 'ywh',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+var res = null;
+// 执行查询
+pool.query('SELECT * FROM user', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+  res = results;
+});
 
 exports.handler = async () => {
   const mySecret = process.env.dburl;
   return {
     statusCode: 200,
-    body: `hello world! I have a ${mySecret}`
+    body: `hello world! I have a ${mySecret} : ${res}`
   };
 };
 
