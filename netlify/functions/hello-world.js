585dc4e85645cb580df4318492a04b3ec8fa0758
gaob13 <gaob13@tsinghua.org.cn>
2025-03-09 00:14:18 +0800

    promise

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,76 +2,31 @@ const mysql = require('mysql2');
 const dburl = process.env.dburl;
 const dbpassword = process.env.dbpassword;
 
-// const pool = mysql.createPool({
-//   host: `${dburl}`,
-//   user: 'gaobin',
-//   password: `${dbpassword}`,
-//   database: 'ywh',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
+const conn = mysql.createConnection({
+    host: `${dburl}`,
+    user: 'gaobin',
+    password: `${dbpassword}`,
+    database: 'ywh'
+});
 
-
-exports.handler = async() => {
-
-    const connection = mysql.createConnection({
-        host: `${dburl}`,
-        user: 'gaobin',
-        password: `${dbpassword}`,
-        database: 'ywh'
-    });
-    var ans;
-    connection.connect((err) => {
-        var ans;
-        if (err) {
-            ans = '数据库连接失败:';
-    	    return {
-		statusCode: 200,
-		body: ans
-	    };
-        }
-    
-        const query = 'SELECT * FROM user';
-        connection.query(query, (error, results, fields) => {
+// 定义一个处理数据库查询的 Promise 函数
+function queryDatabase() {
+    return new Promise((resolve, reject) => {
+        conn.query('SELECT * FROM user', (error, results) => {
             if (error) {
-                ans = '查询出错:';
+                reject(error);
             } else {
-                const jsonResult = JSON.stringify(results);
-                console.log('转换为 JSON 字符串的结果:', jsonResult);
-    	    ans = jsonResult;
+                resolve(results);
             }
-    
-            connection.end((endErr) => {
-                if (endErr) {
-                    ans = '关闭数据库连接出错:';
-                }
-            });
         });
-        return {
-            statusCode: 200,
-            body: `hello world! I have a BUG ${pool}: ${ans}`
-        }
     });
-    return {
-	statusCode: 200,
-	body: ans
-    };
-    
 }
 
-
-exports.handler0 = async () => {
-  var res = {};
-  // 执行查询
-  pool.query('SELECT * FROM user', (error, results, fields) => {
-    if (error) throw error;
-    console.log(results);
-    res.b = JSON.stringify(results);
-  });
-  return {
-    statusCode: 200,
-    body: `hello world! I have a BUG ${pool}: ${res.b}`
-  };
-};
-
+exports.handler = async() => {
+    const ans = await queryDatabase();
+    // console.log(ans);
+    return {
+            statusCode: 200,
+            body: ans
+    }
+}
